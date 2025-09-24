9ca8aaf78543a885337177afe87acb87f10e1a3d
Sara Pechotta <sarapechotta@gmail.com>
2025-09-23 18:24:29 -0700

    Add Users model to database
    Created a new 'users' table with a many-to-many association between users and checklists (i.e., each user may have many checklists, and each checklist may be associated with many users). Also created a users_checklist association table for this relationship.

---
+++ src/users/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from src.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True, index = True)
+    firstName = Column(String(50), nullable = False)
+    lastName = Column(String(50), nullable = False)
+    email = Column(String(100), unique = True, nullable = False, index = True)
+    hashedPassword = Column(String(200), nullable = False)
+
+    users_checklists = relationship("UsersChecklists", back_populates = "user")
+
+
+class UsersChecklists(Base):
+    __tablename__ = "users_checklists"
+
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key = True)
+    checklist_id = Column(Integer, ForeignKey("checklists.id"), primary_key = True)
+
+    user = relationship("User", back_populates = "users_checklists")
+    checklist = relationship("Checklist", back_populates = "users_checklists")
